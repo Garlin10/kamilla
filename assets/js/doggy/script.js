c6f2defa1a4114cc46d76c8dfa76a423a193008e
birokami <kamillabp16@gmail.com>
2025-06-16 17:52:07 +0200

    pet delete

--- assets/js/doggy/script.js
+++ assets/js/doggy/script.js
@@ -1,13 +1,22 @@
-const dog = document.getElementById("dog");
-const counter = document.getElementById("counter");
-
+    const dog = document.getElementById("dog");
+    const counter = document.getElementById("counter");
     async function petDog() {
       const response = await fetch("https://galgyula.hu/pet", {
         method: "GET"
       });
       const data = await response.json();
-      counter.textContent = `Pets: ${data.count}`;
+      console.log(data.count);
+      counter.textContent = "Pets: "+ data.count
     }
-
+    async function numberzero() {
+      const response = await fetch("https://galgyula.hu/delete_petting", {
+        method: "DELETE"
+      });
+      const data = await response.json();
+      console.log(data.count);
+      counter.textContent = "Pets: "+ data.count
+    }
     dog.addEventListener("click", petDog);
+    counter.addEventListener("click", numberzero);
+
 petDog()
--- assets/js/doggy/script1.js
+++
@@ -1,8 +0,0 @@
-    async function petDog() {
-      const response = await fetch("http://127.0.0.1:8000/pet", {
-        method: "GET"
-      });
-      const data = await response.json();
-      console.log(data);
-    }
-petDog()
